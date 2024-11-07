62da791a8c8e88a0dc6cce37fcec54587167b541
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-11-07 18:13:20 +0000

    add clone script

---
+++ clone.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/LMOS-IO/LMOS-vLLM-runner.git
+git clone https://github.com/LMOS-IO/exl2-runner-light.git
+git clone https://github.com/LMOS-IO/LMOS-Router.git
+git clone https://github.com/LMOS-IO/LMOS-config.git
+git clone https://github.com/LMOS-IO/LMOS-database-schema.git
+git clone https://github.com/LMOS-IO/LMOS-Aphrodite-runner.git
