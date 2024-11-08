ab6bc6a3a281cb86471941dd7c8cff1cc0f8d52c
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-11-08 20:08:58 +0000

    fix db clone url

--- clone.sh
+++ clone.sh
@@ -2,5 +2,5 @@ git clone https://github.com/LMOS-IO/LMOS-vLLM-runner.git
 git clone https://github.com/LMOS-IO/exl2-runner-light.git
 git clone https://github.com/LMOS-IO/LMOS-Router.git
 git clone https://github.com/LMOS-IO/LMOS-config.git
-git clone https://github.com/LMOS-IO/LMOS-database-schema.git
+git clone https://github.com/LMOS-IO/LMOS-database.git
 git clone https://github.com/LMOS-IO/LMOS-Aphrodite-runner.git
